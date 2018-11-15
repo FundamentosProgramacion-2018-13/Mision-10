4edf5b00a4dd374bf70bcf5e2c8ddb0d43c7e0fb
Carlos21Reyes <42328882+Carlos21Reyes@users.noreply.github.com>
2018-11-15 12:33:14 -0600

    Update Mision_10.py

--- Mision_10.py
+++ Mision_10.py
@@ -1,7 +1,7 @@
 # Carlos Alberto Reyes Ortiz
 #A01376349
 
-
+import matplotlib.pyplot as plot
 
 
 def listarEquiposOrdenados(nombreArchivo):
@@ -24,7 +24,54 @@ def listarEquiposOrdenados(nombreArchivo):
 
 
 
+def reportarErrorPuntos(nombreArchivo):
+
+    entrada = open(nombreArchivo, "r")
+    entrada.readline()   #Son los titulos, no los queremos
+    entrada.readline()
+
+    listaEquiposError = []
+
+    for linea in entrada:
+        datos = linea.split("&")
+        equipo = datos[0]
+        jg = int(datos[2])
+        je = int(datos[3])
+        pr = int(datos[8])
+        pc = jg*3 + je
+
+        if pr != pc:
+            listaEquiposError.append(equipo)
+
+    entrada.close()
+    return listaEquiposError
+
+
+
+def graficarPuntos(nombreArchivo):
+    entrada = open(nombreArchivo, "r")
+    entrada.readline()  # Son los titulos, no los queremos
+    entrada.readline()
+
+    listaEquipos = []
+    listaPuntos = []
+
+    for linea in entrada:
+        datos = linea.split("&")
+        listaEquipos.append(datos[10])
+        listaPuntos.append(int(datos[8]))
+
+        plot.plot(listaEquipos,listaPuntos)
+
+        plot.show()
+
+
+
 
 def main():
     ordenados = listarEquiposOrdenados("ligaMX.txt")
     print(ordenados)
+
+
+
+main()
