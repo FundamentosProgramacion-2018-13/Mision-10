4e00d25fd8a70878ace7a3741812c1336fbd37d0
A01376718-Silvia <42352922+A01376718-Silvia@users.noreply.github.com>
2018-11-15 15:53:54 -0600

    Add files via upload

---
+++ Mision_10.py
@@ -0,0 +1,142 @@
+# encoding = UTF-8
+# Autor: Silvia Ferman
+# Mision 10
+
+import matplotlib.pyplot as plot          # libreria para graficar
+
+
+# Funcion que muestra los nombres de los equipos en mayuscula y orden alfabetico
+def listarEquiposOrdenados(nombreArchivo):
+    entrada = open(nombreArchivo, "r")
+    entrada.readline()                      # titulo
+    entrada.readline()                      # titulo
+
+    listaEquipos = []
+
+    for linea in entrada:
+        datos = linea.split("&")
+        listaEquipos.append(datos[0].upper())
+
+    listaEquipos.sort()
+    entrada.close()
+    return listaEquipos
+
+
+# Funcion que muestra el equipo y sus puntos que llevan hasta el momento
+def mostrarEquiposPuntos(nombre):
+    entrada = open(nombre, "r")
+    entrada.readline()                     # titulo
+    entrada.readline()                     # titulo
+
+    listaEquipos = []
+
+    for linea in entrada:
+        datos = linea.split("&")
+        listaEquipos.append(datos[0])
+        listaEquipos.append(int(datos[8]))
+
+    entrada.close()
+    return listaEquipos
+
+
+# Funcion que muestra los equipos que han perdido 3 o menos partidos
+def mostrarEquiposPerdedores(nombre):
+    entrada = open(nombre, "r")
+    entrada.readline()                     # titulo
+    entrada.readline()                     # titulo
+
+    listaEquipos = []
+
+    for linea in entrada:
+        datos = linea.split("&")
+        equipo = datos[0]
+        perdidos = int(datos[4])
+        if perdidos <= 3:
+            listaEquipos.append(equipo)
+
+    entrada.close()
+    return listaEquipos
+
+
+# Funcion que muestra los nombres de los equipos que tienen ERROR en su puntaje
+def reportarErrorPuntos(nombre):
+    entrada = open(nombre, "r")
+    entrada.readline()                    # titulo
+    entrada.readline()                    # titulo
+
+    listaEquiposError = []
+
+    for linea in entrada:
+        datos = linea.split("&")
+        equipo = datos[0]
+        jganados = int(datos[2])
+        jempatados = int(datos[3])
+        puntos = int(datos[8])             # los puntos que se reportaron
+        pcalculados = jganados * 3 + jempatados * 1
+        if puntos != pcalculados:
+            listaEquiposError.append(equipo)
+
+    entrada.close()
+    return listaEquiposError
+
+def mostrarDiferenciaGoles(nombre):
+    entrada = open(nombre, "r")
+    entrada.readline()                     # titulo
+    entrada.readline()                     # titulo
+
+    listaEquipos = []
+    listaGoles = []
+
+    for linea in entrada:
+        datos = linea.split("&")
+
+
+    entrada.close()
+    return listaEquipos
+
+
+"def"
+
+# Funcion que muestra una grafica EQUIPOS vs. PUNTOS (solo la dibuja, NO regresa datos)
+def graficarPuntos(nombre):
+    entrada = open(nombre, "r")
+    entrada.readline()
+    entrada.readline()
+
+    listaEquipos= []
+    listaPuntos = []
+
+    for linea in entrada:
+        datos = linea.split("&")
+        listaEquipos.append(datos[0])
+        listaPuntos.append(int(datos[8]))
+
+    # graficar
+    plot.plot(listaEquipos, listaPuntos)
+
+    plot.title("Equipos vs. Puntos")
+    plot.xlabel("EQUIPOS")
+    plot.ylabel("PUNTOS")
+
+    plot.show()
+
+
+def main():
+    ordenados = listarEquiposOrdenados("LigaMX.txt")
+    print(ordenados)
+    errores = reportarErrorPuntos("LigaMX.txt")
+    print(errores)
+    perdedores = mostrarEquiposPerdedores("LigaMX.txt")
+    print(perdedores)
+    puntaje = mostrarEquiposPuntos("LigaMX.txt")
+    print(puntaje)
+    goles = mostrarDiferenciaGoles("LigaMX.txt")
+    print(goles)
+    graficar = graficarPuntos("LigaMX.txt")
+    print(graficar)
+
+
+
+
+
+main()
