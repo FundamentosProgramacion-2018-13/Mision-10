49f532f8aca81a96fca84902a82bca8b1905f20a
lKlausl <42352993+lKlausl@users.noreply.github.com>
2018-11-17 18:20:35 -0600

    Update Mision_10.py

--- Mision_10.py
+++ Mision_10.py
@@ -1,4 +1,111 @@
 #Autor Claudio Mayoral García
 
-def enviar():
-    a = 0
+import matplotlib.pyplot as plot
+
+
+def listarPuntajes(nombreArchivo):
+    entrada = open(nombreArchivo, "r", encoding="UTF-8")
+    entrada.readline()
+    entrada.readline()
+    listaPuntos1 = []
+    for linea in entrada:
+        datos = linea.split("&")
+        for linea in range(1, 4):
+            jg = int(datos[linea])
+            listaPuntos1.append(jg)
+
+    return listaPuntos1
+
+
+def listarEquiposOrdenados(nombreArchivo):
+    entrada = open(nombreArchivo, "r", encoding="UTF-8")
+    entrada.readline()
+    entrada.readline()
+    listaEquipos = []
+    for linea in entrada:
+        datos = linea.split("&")
+        listaEquipos.append(datos[0].upper())
+
+    listaEquipos.sort()
+
+    entrada.close()
+
+    return listaEquipos
+
+
+def reportarErrorPuntos(nombreArchivo):
+    entrada = open(nombreArchivo, "r", encoding="UTF-8")
+    entrada.readline()
+    entrada.readline()
+    listaEquipos = []
+
+    for linea in entrada:
+        datos = linea.split("&")
+        equipo = datos[0]
+        jg = int(datos[2])
+        je = int(datos[3])
+        pr = int(datos[8])
+        pc = jg*3 + je
+        if pr != pc:
+            listaEquipos.append(equipo)
+    entrada.close()
+
+    return listaEquipos
+
+def graficarPuntos(nombreArchivo):
+    entrada = open(nombreArchivo, "r", encoding="UTF-8")
+    entrada.readline()
+    entrada.readline()
+    listaEquipos = []
+    listaPuntos = []
+    for linea in entrada:
+        datos = linea.split("&")
+        listaEquipos.append(datos[0])
+        listaPuntos.append(int(datos[8]))
+    plot.plot(listaEquipos,listaPuntos)
+
+    plot.show()
+
+def mostrarEquiposPerdidos(nombreArchivo):
+    entrada = open(nombreArchivo, "r", encoding="UTF-8")
+    entrada.readline()
+    entrada.readline()
+    listaPerdidos = []
+    for linea in entrada:
+        datos = linea.split("&")
+        perdidos = int(datos[4])
+        if perdidos <= 3:
+            listaPerdidos.append(datos[0])
+    entrada.close()
+    return listaPerdidos
+
+def mostrarEquiposyPuntos(nombreArchivo):
+    entrada = open(nombreArchivo, "r", encoding="UTF-8")
+    entrada.readline()
+    entrada.readline()
+    entablar = {}
+    for linea in entrada:
+        datos = linea.split("&")
+        equipo = datos[0]
+        puntos = int(datos[8])
+        entablar[equipo] = puntos
+    entrada.close()
+    return entablar
+
+
+
+
+
+def main():
+    ordenados = listarEquiposOrdenados("LigaMX.txt")
+    print(ordenados)
+    errores = reportarErrorPuntos("LigaMX.txt")
+    print(errores)
+    graficarPuntos("LigaMX.txt")
+    mostrarEquipoyPunto = mostrarEquiposyPuntos("LigaMX.txt")
+    print(mostrarEquipoyPunto)
+    equiposPerdidos = mostrarEquiposPerdidos("LigaMX.txt")
+    print(equiposPerdidos)
+    puntos = listarPuntajes("LigaMX.txt")
+    print(puntos)
+main()
